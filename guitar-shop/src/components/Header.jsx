787607706f0f65be3570166c1bdb7e833b819bc3
LeoDuffed <carlosmroder@gmail.com>
2025-05-23 08:52:02 -0600

    Haciendo el carrito dinamico

--- guitar-shop/src/components/Header.jsx
+++ guitar-shop/src/components/Header.jsx
@@ -37,13 +37,16 @@ export default function Header({cart}){
                                     <tbody>
                                         {cart.map(guitar => (
                                         
-                                            <tr>
+                                            <tr key={guitar.id}>
                                                 <td>
-                                                    <img className="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra" />
+                                                    <img className="img-fluid" 
+                                                    src= {`/img/${guitar.image}.jpg`}
+                                                    alt="imagen guitarra"
+                                                    />
                                                 </td>
-                                                <td>SRV</td>
+                                                <td>{guitar.name}</td>
                                                 <td className="fw-bold">
-                                                        $299
+                                                        ${guitar.price}
                                                 </td>
                                                 <td className="flex align-items-start gap-4">
                                                     <button
@@ -52,7 +55,7 @@ export default function Header({cart}){
                                                     >
                                                         -
                                                     </button>
-                                                        1
+                                                        {guitar.quantity}
                                                     <button
                                                         type="button"
                                                         className="btn btn-dark"
