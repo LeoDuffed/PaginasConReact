946bb3612f006a4b8a3a2d1cb81569d637b4a589
LeoDuffed <carlosmroder@gmail.com>
2025-06-17 12:55:30 -0600

    ahi va la pagina web :)

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -1,17 +1,84 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState, type ChangeEvent } from "react"
+import { categories } from "../data/categories"
+import type { Activity } from "../types"
+
 export default function Form() {
+
+    const [activity, setActivity] = useState<Activity>({
+        category: 1, 
+        name:'',
+        calories: 0
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+        
+        const isNumbewField = ['category', 'calories'].includes(e.target.id)
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumbewField ? +e.target.value : e.target.value
+        })
+        
+    }
+
+    const isValidActivity = () => {
+        const { name, calories} = activity
+        return name.trim() !== '' && calories > 0
+    }
   return (
     <form
         className="space-y-5 bg-white shadow p-10 rounded-lg"
     >
         <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="category">Categoria:</label>
-            <select 
+            <label htmlFor="category" className="font-bold">Categoria:</label>
+            <select   
                 className="border border-slate-300 p-2 rounded-lg w-full bg-white"
                 id="category"
+                value={activity.category}
+                onChange={handleChange}
                 >
+
+                    {categories.map(categories => (
+                        <option
+                            key={categories.id}
+                            value={categories.id}
+                        >
+                            {categories.name}
+                        </option>
+                    ))}
 
             </select>
         </div>
+
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="name">Actividad:</label>
+            <input 
+                id="name"
+                type="text"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej. Comida: Ensalada, Huevo, Ejercicio: Pesas, Bicicleta"
+                value={activity.name}
+                onChange={handleChange}
+            />
+        </div>
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="calories">Calorias:</label>
+            <input 
+                id="calories"
+                type="number"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej. 530, 300, 100"
+                value={activity.calories === 0 ? '' : activity.calories}
+                onChange={handleChange}
+            />
+        </div>
+        
+        <input
+            type="submit"
+            className="bg-gray-600 hover:bg-gray-700 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-20"
+            value='Guardar'
+            disabled={!isValidActivity()}
+        />
 
     </form>
   )
--- calorie-tracker/src/data/categories.ts
+++ calorie-tracker/src/data/categories.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Category } from "../types"
 
-const categories : Category[] = [
+export const categories : Category[] = [
     {id: 1, name: 'Comida'}, 
     {id: 2, name: 'Ejercicios'}
 ]
