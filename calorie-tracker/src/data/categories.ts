f91093c9a46af42a19a8d8a391f0bce067e6f6a2
LeoDuffed <carlosmroder@gmail.com>
2025-06-16 21:03:28 -0600

    iniciando el proyecto

---
+++ calorie-tracker/src/data/categories.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { Category } from "../types"
+
+const categories : Category[] = [
+    {id: 1, name: 'Comida'}, 
+    {id: 2, name: 'Ejercicios'}
+]
